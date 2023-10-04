d5b61c76587b9018dbc728679a83d9779337dba2
Dharamveer Saw <dharamveersaw32@gmail.com>
2023-10-04 21:51:32 +0530

    Solved Error of runtime not found by creating module file

--- src/main/java/com/tonevellah/demofx1/HelloApplication.java
+++ src/main/java/com/tonevellah/demofx1/HelloApplication.java
@@ -1,4 +1,5 @@
 //start
+// login scene
 package com.tonevellah.demofx1;
 
 import javafx.application.Application;
@@ -6,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,6 +19,10 @@ public class HelloApplication extends Application {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
             Scene scene = new Scene(root);
+            stage.setTitle("TYPE DASH");
+            Image icon = new Image("C:\\Users\\Ganesh\\OneDrive\\Documents\\Dharam\\miniproject\\resources\\TypeDash1.png");
+            stage.getIcons().add(icon);
+
             stage.setScene(scene);
             stage.show();
 

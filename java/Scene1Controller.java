34a9553008dcd8a35ec504b69bc7a58bd5dcb339
Dharamveer Saw <dharamveersaw32@gmail.com>
2023-10-14 00:10:13 +0530

    Successfully Deleted Dark Mode

--- java/Scene1Controller.java
+++ java/Scene1Controller.java
@@ -15,34 +15,35 @@ public class Scene1Controller {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    static public int clr=0;
+//    static public int clr=0;
     static public int lvl=1;
     static public int car=1;
     static public int log=0;
 
-    public void changetheme(ActionEvent event) throws IOException {
-        if(clr==0){
-            clr=1;
-            root = FXMLLoader.load(getClass().getResource("hello-viewb.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        }
-        else{
-            clr=0;
-            root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        }
-    }
+//    public void changetheme(ActionEvent event) throws IOException {
+//        if(clr==0){
+//            clr=1;
+//            root = FXMLLoader.load(getClass().getResource("hello-viewb.fxml"));
+//            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//            scene = new Scene(root);
+//            stage.setScene(scene);
+//            stage.show();
+//        }
+//        else{
+//            clr=0;
+//            root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+//            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//            scene = new Scene(root);
+//            stage.setScene(scene);
+//            stage.show();
+//        }
+//    }
 
     public void loginpage(ActionEvent event) throws IOException {
         System.out.println(clr);
-        if(clr==0)root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
-        else root = FXMLLoader.load(getClass().getResource("Scene12.fxml"));
+//        if(clr==0)
+        root = FXMLLoader.load(getClass().getResource("Scene2.fxml"));
+//        else root = FXMLLoader.load(getClass().getResource("Scene12.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -51,8 +52,9 @@ public class Scene1Controller {
     public void signuppage(ActionEvent event) throws IOException {
 
         //System.out.println("signup");
-        if(clr==0)root = FXMLLoader.load(getClass().getResource("Scene3.fxml"));
-        else root = FXMLLoader.load(getClass().getResource("Scene13.fxml"));
+//        if(clr==0)
+            root = FXMLLoader.load(getClass().getResource("Scene3.fxml"));
+//        else root = FXMLLoader.load(getClass().getResource("Scene13.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
@@ -65,8 +67,9 @@ public class Scene1Controller {
     }
     public void wpmrush(ActionEvent e) throws IOException{
 
-        if(clr==0)root = FXMLLoader.load(getClass().getResource("Scene5.fxml"));
-        else root = FXMLLoader.load(getClass().getResource("Scene15.fxml"));
+//        if(clr==0)
+            root = FXMLLoader.load(getClass().getResource("Scene5.fxml"));
+//        else root = FXMLLoader.load(getClass().getResource("Scene15.fxml"));
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
